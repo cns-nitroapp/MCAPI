74770b4f2ec2c0abbe0b23483839370b258ed95f
Dominik Gralka <43686966+dominik-gralka@users.noreply.github.com>
2021-12-08 22:23:31 +0100

    Implement global router

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var transactionRouter = require('./routes/transactions');
+var globalRouter = require('./routes/global');
 
 var app = express();
 
@@ -22,7 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/transactions', transactionRouter);
-app.use('/users', usersRouter);
+app.use('/global', transactionRouter);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/global.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+// Require createDocument from utils/database
+var createDocument = require('../utils/database').createDocument;
+
+// Post router with transaction data
+router.post('/', function(req, res, next) {
+
+    // Require key from env
+    var key = process.env.API_KEY;
+
+    // Check if key is valid // Authorization header
+    if (req.headers.authorization !== key) {
+        res.status(401).send('Unauthorized');
+    } else {
+
+        // Get data from request
+        var total = req.body.total;
+        var average = req.body.average;
+        var timestamp = new Date().toISOString();
+
+        // Create document
+        var document = {
+            "total": total,
+            "average": average,
+            "timestamp": timestamp,
+        };
+
+        // Create document in database
+        createDocument("global", document, function(error, result) {
+            if (error) {
+                console.log(error);
+                res.status(500).send('Internal Server Error');
+            } else {
+                res.status(200).send('Transaction created');
+            }
+        });
+
+        res.status(200).send('Transaction created');
+
+    }
+});
+
+module.exports = router;
